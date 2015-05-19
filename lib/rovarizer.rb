0168ef61b6dd80d7bbcba3ca588a1545fdead592
Martin Eriksson <Macintrash22@gmail.com>
2015-05-19 15:11:37 +0200

    Skapat en funktion

---
+++ lib/rovarizer.rb
@@ -0,0 +1,7 @@
+def rovarize(string)
+  consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","x","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","X","Z"]
+
+
+  string =
+
+end

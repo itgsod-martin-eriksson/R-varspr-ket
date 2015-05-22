59cda3a107b0322a86f656732231c73e278fec6a
Martin Eriksson <Macintrash22@gmail.com>
2015-05-22 16:20:16 +0200

    Sista Commiten
    Byte namn till cleartext och börjat bygga på en while loop

--- lib/rovarizer.rb
+++ lib/rovarizer.rb
@@ -1,7 +1,12 @@
-def rovarize(string)
+def rovarize(cleartext:)
   consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","x","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","X","Z"]
+  new_letter = "".split(/ /)
+  i = 0
+
+  while i = 0
+    
+  end
 
 
-  string =
 
 end

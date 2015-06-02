f4b5a9327e774187b50c556a0276821ad94f3903
Martin Eriksson <Macintrash22@gmail.com>
2015-06-02 14:19:39 +0200

    Skrivit Funktionen Rovarize

--- lib/rovarizer.rb
+++ lib/rovarizer.rb
@@ -1,12 +1,13 @@
 def rovarize(cleartext:)
   consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","x","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","X","Z"]
-  new_letter = "".split(/ /)
-  i = 0
-
-  while i = 0
-    
+  newword = ""
+  chars = cleartext.split("")
+  chars.each do |char|
+    temp = char
+    if consonants.include?(char.downcase)
+        temp = "#{char}o#{char}"
+    end
+    newword += temp
   end
-
-
-
+  return newword
 end
